23680164d5e91a87b3d3c12dc28cd005f7f3d92f
Razan Armouti <razanarmote@yahoo.com>
2021-09-07 23:21:26 +0300

    class03-PassingFunc

--- horned-animals/src/App.js
+++ horned-animals/src/App.js
@@ -1,20 +1,62 @@
+import React, { Component } from 'react'
 import './App.css';
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+import SelectedBeast from './Components/SelectedBeast';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import dataList from './Components/data.json';
 
 
-function App() {
-  return (
-    <>
-    <Header/>
-<div id="mainDiv"><Main/>
-</div>
-    
-    <Footer/>
-    </>
-    
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      imageUrl: "",
+      title: "",
+      description: "",
+      keyword: "",
+      horns: ""
 
-export default App;
+    }
+
+  }
+  handleClose = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
+    handleOpen = (imageUrl, title,description,keyword,horns) => {
+      this.setState({
+        showModal: true,
+        imageUrl:imageUrl,
+        title:title,
+        description:description,
+        keyword:keyword,
+        horns:horns
+
+      })
+    }
+    render() {
+      return (
+        <>
+          <Header />
+          <Main dataList={dataList} handleOpen={this.handleOpen} />
+          <SelectedBeast handleClose={this.handleClose} 
+                         showModal={this.state.showModal} 
+                         imageUrl={this.state.imageUrl} 
+                         title={this.state.title} 
+                         description={this.state.description} 
+                         keyword={this.state.keyword} 
+                         horns={this.state.horns} />;
+          <Footer />
+        </>
+      )
+    }
+  
+  }
+export default App
+
+
